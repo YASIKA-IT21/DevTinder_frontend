b0a04cfb01d52cee59be915e0df76f22200a2fcc
YASIKA.IT21 <yasika.it21@bitsathy.ac.in>
2025-09-21 23:40:19 +0530

    React Router Dom

---
+++ src/Body.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './NavBar.jsx'
+import Footer from './Footer.jsx'
+import { Outlet } from 'react-router-dom'
+const Body = () => {
+  return (
+    <div>
+        <NavBar />
+        <Outlet/>
+        <Footer/>
+      
+    </div>
+  )
+}
+
+export default Body
